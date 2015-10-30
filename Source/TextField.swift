8104878364bfb2ec82ee33cd4ef3e1801e8a4092
David Keegan <git@davidkeegan.com>
2015-10-29 23:16:48 -0400

    add TextField -> 0.3.1

---
+++ Source/TextField.swift
@@ -0,0 +1,19 @@
+//
+//  TextField.swift
+//  KGNUserInterface
+//
+//  Created by David Keegan on 10/29/15.
+//  Copyright © 2015 David Keegan. All rights reserved.
+//
+
+import UIKit
+import KGNPreferredFontManager
+
+/// Subclass of `PreferredFontTextField`.
+public class TextField: PreferredFontTextField {
+
+    override public func setup() {
+        super.setup()
+    }
+
+}
