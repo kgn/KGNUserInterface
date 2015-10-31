15cbf0a7cd15019eefa7632a4f9eda7b4c873264
David Keegan <git@davidkeegan.com>
2015-10-30 20:27:32 -0400

    Added a way to set the placeholder text color -> 0.3.3

--- Source/TextField.swift
+++ Source/TextField.swift
@@ -10,13 +10,69 @@ import UIKit
 import KGNPreferredFontManager
 
 /// Subclass of `PreferredFontTextField`. This implementation also allows
-/// the text color to be set by `Label.appearance().textColor`.
+/// the text color to be set by `Label.appearance().textColor` and adds
+/// a property for setting the color of the placeholder text.
 public class TextField: PreferredFontTextField {
+
+    public override var textStyle: String {
+        didSet {
+            self.placeholderLabel.textStyle = self.textStyle
+        }
+    }
+
+    public override var preferredFontManager: PreferredFontManager? {
+        didSet {
+            self.placeholderLabel.preferredFontManager = self.preferredFontManager
+        }
+    }
+
+    private var privatePlaceholder: String? {
+        didSet {
+            self.placeholderLabel.text = self.privatePlaceholder
+        }
+    }
+
+    public override var placeholder: String? {
+        get {
+            return self.privatePlaceholder
+        }
+
+        set(newValue) {
+            self.privatePlaceholder = newValue
+        }
+    }
+
+    /// The color of the placeholder text.
+    var placeholderColor: UIColor? {
+        didSet {
+            self.placeholderLabel.textColor = self.placeholderColor
+        }
+    }
+
+    private lazy var placeholderLabel: Label = {
+        let label = Label(textStyle: self.textStyle)
+        label.textColor = self.placeholderColor
+        label.text = self.placeholder
+        return label
+    }()
 
     override public func setup() {
         super.setup()
 
         self.textColor = TextField.appearance().textColor
+        self.addTarget(self, action: "textDidChange:", forControlEvents: .EditingChanged)
+
+        self.addSubview(self.placeholderLabel)
+        self.placeholderLabel.centerVerticallyInSuperview()
+        self.placeholderLabel.pinToLeftEdgeOfSuperview()
+    }
+
+    @objc private func textDidChange(sender: AnyObject) {
+        if self.text == nil || self.text == "" {
+            self.placeholderLabel.hidden = false
+        } else {
+            self.placeholderLabel.hidden = true
+        }
     }
 
 }
