958c6cc2fbe9dab93b496c006d7abf1b9a6d6e2a
David Keegan <git@davidkeegan.com>
2015-11-03 09:23:02 -0500

    add title and image to button: 0.3.8

--- Source/Button.swift
+++ Source/Button.swift
@@ -12,6 +12,28 @@ import KGNPreferredFontManager
 /// Subclass of `PreferredFontButton`.
 public class Button: PreferredFontButton {
 
+    /// Helper property that sets the button title for the `Normal` state
+    public var title: String? {
+        get {
+            return self.titleForState(.Normal)
+        }
+
+        set(title) {
+            self.setTitle(title, forState: .Normal)
+        }
+    }
+
+    /// Helper property that sets the button image for the `Normal` state
+    public var image: UIImage? {
+        get {
+            return self.imageForState(.Normal)
+        }
+
+        set(image) {
+            self.setImage(image, forState: .Normal)
+        }
+    }
+
     override public func setup() {
         super.setup()
     }
